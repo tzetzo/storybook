59a02e714c47dee7df62750add552169816c5f1b
Tzvetan Marinov <tmarinov@stamus-networks.com>
2025-07-22 07:49:53 +0300

    button size prop added

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -9,8 +9,10 @@ const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
   args: {
-    children: 'Button',
+    children: 'Button', // Default value for the children prop, applies to all stories
     disabled: false, // Default value for the disabled prop below
+    variant: 'primary', // Default value for the variant prop as it is stated in the Component
+    size: 'medium', // Default value for the size prop as it is stated in the Component
   },
   argTypes: {
     disabled: {
@@ -18,12 +20,19 @@ const meta: Meta<typeof Button> = {
       control: 'boolean',
       description: 'Disables the button',
     },
+    variant: {
+      control: 'select',
+    },
+    size: {
+      control: 'select',
+    },
   },
 };
 
 export default meta;
 
 // Each named export is a story
+// We can intentionally omit the 'Primary' variant story because it is the default and it will be tested with 'Small' & 'Large' stories anyway!!!
 export const Primary: Story = {
   args: {
     variant: 'primary',
@@ -44,5 +53,35 @@ export const Secondary: Story = {
 export const Destructive: Story = {
   args: {
     variant: 'destructive',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+// We intentionally omit the 'Medium' size story because it is the default and it will be tested with 'Primary', 'Secondary' & 'Destructive' stories anyway!!!
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Dark: Story = {
+  parameters: {
+    themes: {
+      themeOverride: 'dark',
+    },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1', // This will apply the mobile viewport to this story
+    },
   },
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -6,15 +6,17 @@ import styles from './button.module.css';
 // ComponentProps<'button'> ensures that ButtonProps includes all props of a standard button
 type ButtonProps = ComponentProps<'button'> & {
   variant?: 'primary' | 'secondary' | 'destructive'; // Prop specific to our button component
+  size?: 'small' | 'medium' | 'large';
 };
 
-export const Button = ({ variant = 'primary', className, ...props }: ButtonProps) => {
-  // using clsx to conditionally apply styles based on the variant prop
+export const Button = ({ variant = 'primary', size = 'medium', ...props }: ButtonProps) => {
+  // using clsx to conditionally apply styles based on the variant & size props
   const classes = clsx(
+    'bg-indigo-500 hover:bg-indigo-400',
     styles.button,
     styles[variant], // This will dynamically access styles based on the variant prop
-    className, // provides the user opportunity to add additional classes
+    styles[size],
   );
 
-  return <button {...props} className={classes} />;
+  return <button className={classes} {...props} />; // inserting {...props} to pass down all other button attributes like onClick, disabled, etc. and to allow overriding styles including with className
 };
